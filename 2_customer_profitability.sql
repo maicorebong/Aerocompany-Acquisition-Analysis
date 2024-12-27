38771277672d4be961424e91d548fdc5480dc484
Maico Rebong <maicorebong@gmail.com>
2024-12-27 16:35:30 -0500

    Update 2_customer_profitability.sql

--- 2_customer_profitability.sql
+++ 2_customer_profitability.sql
@@ -4,7 +4,7 @@ SELECT
     t.customer AS customer_name,
     ROUND(SUM(t.revenue), 2) AS total_revenue,
     ROUND(SUM(t.profit_or_loss), 2) AS total_profit,
-    ROUND(SUM(t.profit_or_loss) * 100.0 / NULLIF(SUM(t.revenue), 0), 2) AS profit_margin_percentage,
+    ROUND((SUM(t.profit_or_loss) / NULLIF(SUM(t.revenue), 0)) * 100.0, 2) AS profit_margin_percentage,
     ROUND(SUM(i.value_of_inv_onhand), 2) AS inventory_holding_cost,
     COUNT(DISTINCT i.part_number) AS distinct_products_sold
 FROM transactions_sales t
