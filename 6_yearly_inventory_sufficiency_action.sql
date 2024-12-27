1fa6a9bca7209f8233de65792a85806e49c49a01
Maico Rebong <maicorebong@gmail.com>
2024-12-26 23:26:52 -0500

    Update 6_yearly_inventory_sufficiency_action.sql

--- 6_yearly_inventory_sufficiency_action.sql
+++ 6_yearly_inventory_sufficiency_action.sql
@@ -2,16 +2,16 @@
 
 CREATE TABLE yearly_inventory_sufficiency_action AS
 SELECT 
-    inventory.year AS inventory_year,
-    inventory.part_number,
-    SUM(inventory.inv_oh) AS total_inventory_on_hand,
-    SUM(inventory.to_make) AS total_to_make,
-    inventory.inventory_status,
+    i.year AS inventory_year,
+    i.part_number,
+    SUM(i.inv_oh) AS total_inventory_on_hand,
+    SUM(i.to_make) AS total_to_make,
+    i.inventory_status,
     CASE
-        WHEN SUM(inventory.to_make) = 0 THEN 'No Additional Inventory Needed'
-        WHEN SUM(inventory.to_make) > SUM(inventory.inv_oh) THEN 'Additional Inventory Needed'
+        WHEN SUM(i.to_make) = 0 THEN 'No Additional Inventory Needed'
+        WHEN SUM(i.to_make) > SUM(i.inv_oh) THEN 'Additional Inventory Needed'
         ELSE 'Sufficient Inventory Available'
     END AS sufficiency_status_action
-FROM inventory
-GROUP BY inventory.part_number, inventory.year, inventory.inventory_status, inventory.last_sale_last_order
+FROM inventory i
+GROUP BY i.part_number, i.year, i.inventory_status, i.last_sale_last_order
 ORDER BY total_to_make DESC;
