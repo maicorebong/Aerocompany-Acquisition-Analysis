5110f1782ffd00e289e3a33b75c71a966faa4b45
Maico Rebong <maicorebong@gmail.com>
2024-12-27 16:57:34 -0500

    Update 3_material_contribution_margin.sql

--- 3_material_contribution_margin.sql
+++ 3_material_contribution_margin.sql
@@ -1,12 +1,22 @@
 -- Contribution of Material to Total Revenue
 
-CREATE TABLE material_contribution_margin AS
+-- calculate total annual revenue for each year
+WITH yearly_totals AS (
+    SELECT 
+        year AS transaction_year,
+        SUM(revenue) AS total_annual_revenue
+    FROM transactions_sales
+    GROUP BY year
+)
+
+-- calculate revenue contribution of each material
 SELECT
-    year AS transaction_year,
-    material,
-    ROUND(SUM(revenue), 2) AS total_revenue,
-    ROUND(SUM(revenue) * 100.0 / 
-          (SELECT SUM(revenue) FROM transactions_sales WHERE year = t.year), 2) AS revenue_percentage
+    t.year AS transaction_year,
+    t.material,
+    ROUND(SUM(t.revenue), 2) AS total_revenue,
+    ROUND((SUM(t.revenue) / y.total_annual_revenue) * 100, 2) AS revenue_percentage
 FROM transactions_sales t
-GROUP BY year, material
-ORDER BY transaction_year, total_revenue DESC;
+JOIN yearly_totals y
+ON t.year = y.transaction_year
+GROUP BY t.year, t.material, y.total_annual_revenue
+ORDER BY year, total_revenue DESC;
